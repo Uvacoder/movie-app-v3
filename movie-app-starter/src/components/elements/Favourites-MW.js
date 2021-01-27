78d8dcc5bc8deec53de415a347866faa1d46d6cd
Mina Bui <mbui14@my.bcit.ca>
2021-01-27 15:25:03 -0800

    cleaning up

--- movie-app-starter/src/components/elements/Favourites-MW.js
+++ movie-app-starter/src/components/elements/Favourites-MW.js
@@ -1,6 +1,6 @@
-  // Favourites
+// Favourites
 import { useState, useEffect } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useGlobal from '../store/globalAppState';
 import SixColGrid from './SixColGrid-MW';
 /*
@@ -12,7 +12,7 @@ import Movies from '/Movies';
 const Favourites = () => {
 
   const [globalState, globalActions] = useGlobal();
-  
+
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
@@ -21,36 +21,36 @@ const Favourites = () => {
   }, [globalActions]);
 
 
-  
-  return (
-      <section>
-        <h2>Favourite Movies</h2>
 
-        
-        <div className="favourite-tutorial-icons">
+  return (
+    <section class="favourites-section">
+
+      <Link to={'/'} className="go-back">&#5176; Back</Link>
+
+      <h2>Favourite Movies</h2>
+
+      {globalState.favs.length === 0 ?
+        <div className="fave-movies-text">
+          <div className="favourite-tutorial-icons">
             <img src="/../images/add icon.png" alt="Add Icon" className="favourite_icon"></img>
             <img src="/../images/arrow_icon.png" alt="Arrow Icon" className="favourite_icon"></img>
             <img src="/../images/favourite_icon.png" alt="Favourite Icon" className="favourite_icon"></img>
-        </div> 
-    
-       
-        {globalState.favs.length === 0 ?
-          <div className="fave-movies-text">
-            <p>Uh-oh, looks like you haven't chosen any movies yet!</p>
-            <p>Feel free to return back to the home page and explore more movies!</p>
-            <p>To add a favourite movie on the home page, you can click on the <img src="/images/favourite_icon.png" alt="Favourite Icon" className="favourite_icon"></img> while hovering or by clicking on the movie poster.</p>
-            <p>You can also find the <img src="/images/favourite_icon.png" alt="Favourite Icon" className="favourite_icon"></img> when you click on an individual movie to learn more or watch a trailer.</p>
           </div>
-          :
-          (ready && 
-            <div className="home-grid">
-                <SixColGrid movies={globalState.favs} />     
-             </div>   
-        ) 
-        }
+          <p>Uh-oh, looks like you haven't chosen any movies yet!</p>
+          <p>Feel free to return back to the home page and explore more movies!</p>
+          <p>To add a favourite movie on the home page, you can click on the <img src="/images/favourite_icon.png" alt="Favourite Icon" className="favourite_icon"></img> while hovering or by clicking on the movie poster.</p>
+          <p>You can also find the <img src="/images/favourite_icon.png" alt="Favourite Icon" className="favourite_icon"></img> when you click on an individual movie to learn more or watch a trailer.</p>
+        </div>
+        :
+        (ready &&
+          <div className="home-grid">
+            <SixColGrid movies={globalState.favs} />
+          </div>
+        )
+      }
 
-      </section>
-   
+    </section>
+
   );
 
 
--- movie-app-starter/src/components/elements/MovieThumb-MW.js
+++ movie-app-starter/src/components/elements/MovieThumb-MW.js
@@ -16,28 +16,27 @@ const MovieThumb = ({ movie, movieId }) => {
     return (
         <div className="moviethumb">
             <div className="movie">
-               
-                    <Link to={{ 
-                        // grabs the movie id so that it can be grabbed with link to show on URL
-                        pathname: `/single-movie/${movie.id}`, 
-                        // sends in the movie name to be shwon in header
-                        movieName: `${movie.name}` 
-                    }}>
-                    
-                       {movie.poster_path === null ? <img src="./images/no_image.jpg" /> :  <img src={`${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`} alt={movie.title} />}
-                        <div className="moviethumb-details">
-                            <div className="moviethumb-title">{movie.original_title}</div>
-                            <div className="moviethumb-overview">{movie.overview}</div>
-                            <div className="moviethumb-genre">[{movie.name, movie.id}]</div>
-                            <div className="moviethumb-release">[{movie.release_date}]</div>
-                        </div>
-                        <div className="moviethumb-moreinfo-rating">
-                            <div className="moviethumb-moreinfo"><button className="moviethumb-moreinfo-btn"><p>More Info</p></button></div>
-                            <div className="moviethumb-rating">{movie.vote_average}</div>
-                        </div>
-                    </Link>
-                   
-                
+
+                <Link to={{
+                    // grabs the movie id so that it can be grabbed with link to show on URL
+                    pathname: `/single-movie/${movie.id}`,
+                    // sends in the movie name to be shwon in header
+                    movieName: `${movie.name}`
+                }}>
+
+                    {movie.poster_path === null ? <img src="./images/no_image.jpg" /> : <img src={`${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`} alt={movie.title} />}
+                    <div className="moviethumb-details">
+                        <div className="moviethumb-title">{movie.original_title}</div>
+                        <div className="moviethumb-overview">{movie.overview}</div>
+                        <div className="moviethumb-release">[{movie.release_date}]</div>
+                    </div>
+                    <div className="moviethumb-moreinfo-rating">
+                        <div className="moviethumb-moreinfo"><button className="moviethumb-moreinfo-btn"><p>More Info</p></button></div>
+                        <div className="moviethumb-rating">{movie.vote_average}</div>
+                    </div>
+                </Link>
+
+
             </div>
         </div>
     )
