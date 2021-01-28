68ace2f1ae8f8660317a9f94962613fc5c07756b
Mina Bui <mbui14@my.bcit.ca>
2021-01-27 17:49:14 -0800

    Update Header.js

--- movie-app-starter/src/components/elements/Header.js
+++ movie-app-starter/src/components/elements/Header.js
@@ -8,7 +8,11 @@ const Header = () => {
   return (
     <div className="movie-site-header">
       <div className="movie-site-header-content">
+<<<<<<< Updated upstream
         <Link to="/"><img className="movie-site-logo" src="../../images/filmdex_logo_large.png" alt="React Movie Logo" /></Link>
+=======
+        <Link to="/"><img className="movie-site-logo" src="../../images/database_logo_green.png" alt="React Movie Logo" /></Link>
+>>>>>>> Stashed changes
         <div className="nav-bar-styling">
           <NavBar />
         </div>
